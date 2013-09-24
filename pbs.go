a617d784de2c1ce561b42717b20b78667875ac67
Jonathan Barber <jonathan.barber@gmail.com>
2013-09-24 11:12:36 +0100

    Many, many changes

--- pbs.go
+++ pbs.go
@@ -1,46 +1,28 @@
-package pbs
-
-/* pbs functions:
+// Package pbs provides an interface to the C-based TORQUE library.
+// The functions present in this package are a thin wrapper around these
+// library functions and as such the TORQUE library provides documentation on
+// their usage.
+//
+// The TORQUE library is not thread safe, particulary when it comes to
+// reporting errors, and therefore problems *might* arise if you use this
+// package with goroutines.
+//
+// The following functions have not yet been implemented:
+/*
    pbs_alterjob
    pbs_alterjobasync
-   pbs_checkpointjob    -  done
-   pbs_connect          -  done
-   pbs_default          -  done
-   pbs_deljob           -  done
-   pbs_disconnect       -  done
-   pbs_fbserver         -  done
-   pbs_get_server_list  -  done
-   pbs_geterrmsg        -  done
    pbs_gpumode
    pbs_gpureset
-   pbs_holdjob          -  done (untested)
-   pbs_locjob           -  done
    pbs_manager
    pbs_movejob
-   pbs_msgjob           -  done (untested)
-   pbs_orderjob         -  done (untested)
-   pbs_rerunjob         -  done
    pbs_rescquery
-   avail
-   totpool              -  done
-   usepool              -  done
    pbs_rescreserve
-   pbs_rlsjob           -  done (untested)
-   pbs_runjob           -  done (untested)
    pbs_asyncrunjob
-   pbs_selectjob        -  partially done
    pbs_selstat
-   pbs_sigjob           -  done
    pbs_sigjobasync
    pbs_stagein
-   pbs_statjob          -  partially done
-   pbs_statnode         -  partially done
-   pbs_statque          -  partially done
-   pbs_statserver       -  partially done
-   pbs_strerror         -  done
-   pbs_submit           -  partially done
-   pbs_terminate        -  done
 */
+package pbs
 
 // #cgo CFLAGS: -g
 // #cgo LDFLAGS: -ltorque
@@ -53,34 +35,168 @@ import (
 	"unsafe"
 )
 
+// BatchStatus represents the batch_status structure
 type BatchStatus struct {
 	Name       string
 	Text       string
 	Attributes []Attrib
 }
+
+// Attrib represents the attrl and attropl structures
 type Attrib struct {
 	Name     string
 	Resource string
 	Value    string
+	Op       Operator
 }
+
+// Manner defines how the server should be terminated
 type Manner int
+// Hold defines the type of job hold to place on a job
 type Hold string
+// MessageStream which output stream should be written to
 type MessageStream int
+// Operator defines types of logical comparator
+type Operator int
 
 const (
-	SHUT_IMMEDIATE Manner        = C.SHUT_IMMEDIATE
-	SHUT_DELAY     Manner        = C.SHUT_DELAY
-	USER_HOLD      Hold          = C.USER_HOLD
-	OTHER_HOLD     Hold          = C.OTHER_HOLD
-	SYSTEM_HOLD    Hold          = C.SYSTEM_HOLD
-	MSG_ERR        MessageStream = C.MSG_ERR
-	MSG_OUT        MessageStream = C.MSG_OUT
+	SHUT_IMMEDIATE                 Manner        = C.SHUT_IMMEDIATE
+	SHUT_DELAY                     Manner        = C.SHUT_DELAY
+	USER_HOLD                      Hold          = C.USER_HOLD
+	OTHER_HOLD                     Hold          = C.OTHER_HOLD
+	SYSTEM_HOLD                    Hold          = C.SYSTEM_HOLD
+	MSG_ERR                        MessageStream = C.MSG_ERR
+	MSG_OUT                        MessageStream = C.MSG_OUT
+	ATTR_a                         string        = C.ATTR_a
+	ATTR_c                         string        = C.ATTR_c
+	ATTR_e                         string        = C.ATTR_e
+	ATTR_f                         string        = C.ATTR_f
+	ATTR_g                         string        = C.ATTR_g
+	ATTR_h                         string        = C.ATTR_h
+	ATTR_j                         string        = C.ATTR_j
+	ATTR_k                         string        = C.ATTR_k
+	ATTR_l                         string        = C.ATTR_l
+	ATTR_m                         string        = C.ATTR_m
+	ATTR_o                         string        = C.ATTR_o
+	ATTR_p                         string        = C.ATTR_p
+	ATTR_q                         string        = C.ATTR_q
+	ATTR_r                         string        = C.ATTR_r
+	ATTR_t                         string        = C.ATTR_t
+	ATTR_array_id                  string        = C.ATTR_array_id
+	ATTR_u                         string        = C.ATTR_u
+	ATTR_v                         string        = C.ATTR_v
+	ATTR_A                         string        = C.ATTR_A
+	ATTR_args                      string        = C.ATTR_args
+	ATTR_M                         string        = C.ATTR_M
+	ATTR_N                         string        = C.ATTR_N
+	ATTR_S                         string        = C.ATTR_S
+	ATTR_depend                    string        = C.ATTR_depend
+	ATTR_inter                     string        = C.ATTR_inter
+	ATTR_stagein                   string        = C.ATTR_stagein
+	ATTR_stageout                  string        = C.ATTR_stageout
+	ATTR_jobtype                   string        = C.ATTR_jobtype
+	ATTR_submit_host               string        = C.ATTR_submit_host
+	ATTR_init_work_dir             string        = C.ATTR_init_work_dir
+	ATTR_ctime                     string        = C.ATTR_ctime
+	ATTR_exechost                  string        = C.ATTR_exechost
+	ATTR_execport                  string        = C.ATTR_execport
+	ATTR_mtime                     string        = C.ATTR_mtime
+	ATTR_qtime                     string        = C.ATTR_qtime
+	ATTR_session                   string        = C.ATTR_session
+	ATTR_euser                     string        = C.ATTR_euser
+	ATTR_egroup                    string        = C.ATTR_egroup
+	ATTR_hashname                  string        = C.ATTR_hashname
+	ATTR_hopcount                  string        = C.ATTR_hopcount
+	ATTR_security                  string        = C.ATTR_security
+	ATTR_sched_hint                string        = C.ATTR_sched_hint
+	ATTR_substate                  string        = C.ATTR_substate
+	ATTR_name                      string        = C.ATTR_name
+	ATTR_owner                     string        = C.ATTR_owner
+	ATTR_used                      string        = C.ATTR_used
+	ATTR_state                     string        = C.ATTR_state
+	ATTR_queue                     string        = C.ATTR_queue
+	ATTR_server                    string        = C.ATTR_server
+	ATTR_maxrun                    string        = C.ATTR_maxrun
+	ATTR_maxreport                 string        = C.ATTR_maxreport
+	ATTR_total                     string        = C.ATTR_total
+	ATTR_comment                   string        = C.ATTR_comment
+	ATTR_cookie                    string        = C.ATTR_cookie
+	ATTR_qrank                     string        = C.ATTR_qrank
+	ATTR_altid                     string        = C.ATTR_altid
+	ATTR_etime                     string        = C.ATTR_etime
+	ATTR_exitstat                  string        = C.ATTR_exitstat
+	ATTR_forwardx11                string        = C.ATTR_forwardx11
+	ATTR_submit_args               string        = C.ATTR_submit_args
+	ATTR_tokens                    string        = C.ATTR_tokens
+	ATTR_netcounter                string        = C.ATTR_netcounter
+	ATTR_umask                     string        = C.ATTR_umask
+	ATTR_start_time                string        = C.ATTR_start_time
+	ATTR_start_count               string        = C.ATTR_start_count
+	ATTR_checkpoint_dir            string        = C.ATTR_checkpoint_dir
+	ATTR_checkpoint_name           string        = C.ATTR_checkpoint_name
+	ATTR_checkpoint_time           string        = C.ATTR_checkpoint_time
+	ATTR_checkpoint_restart_status string        = C.ATTR_checkpoint_restart_status
+	ATTR_restart_name              string        = C.ATTR_restart_name
+	ATTR_comp_time                 string        = C.ATTR_comp_time
+	ATTR_reported                  string        = C.ATTR_reported
+	ATTR_intcmd                    string        = C.ATTR_intcmd
+	ATTR_P                         string        = C.ATTR_P
+	ATTR_node_exclusive            string        = C.ATTR_node_exclusive
+	ATTR_exec_gpus                 string        = C.ATTR_exec_gpus
+	ATTR_J                         string        = C.ATTR_J
+	SET                            Operator      = C.SET
+	UNSET                          Operator      = C.UNSET
+	INCR                           Operator      = C.INCR
+	DECR                           Operator      = C.DECR
+	EQ                             Operator      = C.EQ
+	NE                             Operator      = C.NE
+	GE                             Operator      = C.GE
+	GT                             Operator      = C.GT
+	LE                             Operator      = C.LE
+	LT                             Operator      = C.LT
+	DFLT                           Operator      = C.DFLT
+	MERGE                          Operator      = C.MERGE
+	INCR_OLD                       Operator      = C.INCR_OLD
 )
+
+func attrib2attribl(attribs []Attrib) *C.struct_attrl {
+	// Empty array returns null pointer
+	if len(attribs) == 0 {
+		return nil
+	}
+
+	first := &C.struct_attrl{
+		value:    C.CString(attribs[0].Value),
+		resource: C.CString(attribs[0].Resource),
+		name:     C.CString(attribs[0].Name),
+        op:       uint32(attribs[0].Op),
+	}
+	tail := first
+
+	for _, attr := range attribs[1:len(attribs)] {
+		tail.next = &C.struct_attrl{
+			value:    C.CString(attr.Value),
+			resource: C.CString(attr.Resource),
+			name:     C.CString(attr.Name),
+            op:       uint32(attribs[0].Op),
+		}
+	}
+
+	return first
+}
+
+func freeattribl(attrl *C.struct_attrl) {
+	for p := attrl; p != nil; p = p.next {
+		C.free(unsafe.Pointer(p.name))
+		C.free(unsafe.Pointer(p.value))
+		C.free(unsafe.Pointer(p.resource))
+	}
+}
 
 func get_pbs_batch_status(batch_status *_Ctype_struct_batch_status) (batch []BatchStatus) {
 	for batch_status != nil {
 		temp := []Attrib{}
-		for attr := batch_status.attribs; attr.next != nil; attr = attr.next {
+		for attr := batch_status.attribs; attr != nil; attr = attr.next {
 			temp = append(temp, Attrib{
 				Name:     C.GoString(attr.name),
 				Resource: C.GoString(attr.resource),
@@ -132,22 +248,26 @@ func Pbs_checkpointjob(handle int, id string, extend string) error {
 	return nil
 }
 
+// Pbs_connect makes a connection to server, or if server is an empty string, the default server. The returned handle is used by subsequent calls to the functions in this package to identify the server.
 func Pbs_connect(server string) (int, error) {
 	str := C.CString(server)
 	defer C.free(unsafe.Pointer(str))
-	handle := C.pbs_connect(str)
 
+	handle := C.pbs_connect(str)
 	if handle < 0 {
 		return 0, errors.New(Pbs_strerror(int(C.pbs_errno)))
 	}
+
 	return int(handle), nil
 }
 
+// Pbs_default reports the default torque server
 func Pbs_default() string {
-    // char* from pbs_default is statically allocated, so can't be freed
-    return C.GoString(C.pbs_default())
+	// char* from pbs_default is statically allocated, so can't be freed
+	return C.GoString(C.pbs_default())
 }
 
+// Pbs_deljob deletes a job on the server
 func Pbs_deljob(handle int, id string, extend string) error {
 	e := C.CString(extend)
 	defer C.free(unsafe.Pointer(e))
@@ -172,18 +292,18 @@ func Pbs_disconnect(handle int) error {
 }
 
 func Pbs_fbserver() string {
-    // char* from pbs_fbserver is statically allocated, so can't be freed
+	// char* from pbs_fbserver is statically allocated, so can't be freed
 	return C.GoString(C.pbs_fbserver())
 }
 
 func Pbs_get_server_list() string {
-    // char* from pbs_get_server_list is statically allocated, so can't be freed
+	// char* from pbs_get_server_list is statically allocated, so can't be freed
 	return C.GoString(C.pbs_get_server_list())
 }
 
 func Pbs_geterrmsg(handle int) string {
 	s := C.pbs_geterrmsg(C.int(handle))
-	defer C.free(unsafe.Pointer(s))
+	// char* from pbs_geterrmsg is statically allocated, so can't be freed
 	return C.GoString(s)
 }
 
@@ -267,6 +387,16 @@ func Pbs_rerunjob(handle int, id string, extend string) error {
 	return nil
 }
 
+func Avail(handle int, resc string) string {
+	r := C.CString(resc)
+	defer C.free(unsafe.Pointer(r))
+
+	c := C.avail(C.int(handle), r)
+	//defer C.free(unsafe.Pointer(c))
+
+	return C.GoString(c)
+}
+
 func Totpool(handle int, update int) (int, error) {
 	ret := int(C.totpool(C.int(handle), C.int(update)))
 	if ret < 0 {
@@ -318,14 +448,20 @@ func Pbs_runjob(handle int, id string, location string, extend string) error {
 	return nil
 }
 
-func Pbs_selectjob(handle int) ([]string, error) {
-    // Torque's implementation of pbs_selectjob() is hideously broken and it
-    // only works accidentally - they allocate a single block of memory (which is
-    // oversized) for the jobids and then copy them into it.
-    // Because only a single malloc() is used you only need to free() the
-    // char** returned by pbs_selectjob().
+// Pbs_selectjob returns a list of jobs
+func Pbs_selectjob(handle int, attrib []Attrib, extend string) ([]string, error) {
+	// Torque's implementation of pbs_selectjob() is broken and only works
+	// accidentally - they allocate a single block of memory (which is
+	// oversized) for the jobids and then copy them into it.
+	// Because only a single malloc() is used you only need to free() the
+	// char** returned by pbs_selectjob().
+	e := C.CString(extend)
+	defer C.free(unsafe.Pointer(e))
 
-	p := C.pbs_selectjob(C.int(handle), nil, nil)
+	a := attrib2attribl(attrib)
+	defer freeattribl(a)
+
+	p := C.pbs_selectjob(C.int(handle), (*C.struct_attropl)(unsafe.Pointer(a)), e)
 	if p == nil {
 		return nil, errors.New(Pbs_strerror(int(C.pbs_errno)))
 	}
@@ -352,27 +488,17 @@ func Pbs_sigjob(handle int, id string, signal string, extend string) error {
 	return nil
 }
 
-func Pbs_statjob(handle int, id string) ([]BatchStatus, error) {
-	s := C.CString(id)
-	defer C.free(unsafe.Pointer(s))
+func Pbs_statjob(handle int, id string, attribs []Attrib, extend string) ([]BatchStatus, error) {
+	i := C.CString(id)
+	defer C.free(unsafe.Pointer(i))
 
-	batch_status := C.pbs_statjob(C.int(handle), s, nil, nil)
+	e := C.CString(extend)
+	defer C.free(unsafe.Pointer(e))
 
-	if batch_status == nil {
-		return nil, errors.New(Pbs_strerror(int(C.pbs_errno)))
-	}
-	defer C.pbs_statfree(batch_status)
+	a := attrib2attribl(attribs)
+	defer freeattribl(a)
 
-	batch := get_pbs_batch_status(batch_status)
-
-	return batch, nil
-}
-
-func Pbs_statnode(handle int, id string) ([]BatchStatus, error) {
-	s := C.CString(id)
-	defer C.free(unsafe.Pointer(s))
-
-	batch_status := C.pbs_statnode(C.int(handle), s, nil, nil)
+	batch_status := C.pbs_statjob(C.int(handle), i, a, e)
 
 	if batch_status == nil {
 		return nil, errors.New(Pbs_strerror(int(C.pbs_errno)))
@@ -384,8 +510,17 @@ func Pbs_statnode(handle int, id string) ([]BatchStatus, error) {
 	return batch, nil
 }
 
-func Pbs_statque(handle int) ([]BatchStatus, error) {
-	batch_status := C.pbs_statque(C.int(handle), nil, nil, nil)
+func Pbs_statnode(handle int, id string, attribs []Attrib, extend string) ([]BatchStatus, error) {
+	i := C.CString(id)
+	defer C.free(unsafe.Pointer(i))
+
+	a := attrib2attribl(attribs)
+	defer freeattribl(a)
+
+	e := C.CString(extend)
+	defer C.free(unsafe.Pointer(e))
+
+	batch_status := C.pbs_statnode(C.int(handle), i, a, e)
 
 	if batch_status == nil {
 		return nil, errors.New(Pbs_strerror(int(C.pbs_errno)))
@@ -397,8 +532,36 @@ func Pbs_statque(handle int) ([]BatchStatus, error) {
 	return batch, nil
 }
 
-func Pbs_statserver(handle int) ([]BatchStatus, error) {
-	batch_status := C.pbs_statserver(C.int(handle), nil, nil)
+func Pbs_statque(handle int, id string, attribs []Attrib, extend string) ([]BatchStatus, error) {
+	i := C.CString(id)
+	defer C.free(unsafe.Pointer(i))
+
+	a := attrib2attribl(attribs)
+	defer freeattribl(a)
+
+	e := C.CString(extend)
+	defer C.free(unsafe.Pointer(e))
+
+	batch_status := C.pbs_statque(C.int(handle), i, a, e)
+
+	if batch_status == nil {
+		return nil, errors.New(Pbs_strerror(int(C.pbs_errno)))
+	}
+	defer C.pbs_statfree(batch_status)
+
+	batch := get_pbs_batch_status(batch_status)
+
+	return batch, nil
+}
+
+func Pbs_statserver(handle int, attribs []Attrib, extend string) ([]BatchStatus, error) {
+	a := attrib2attribl(attribs)
+	defer freeattribl(a)
+
+	e := C.CString(extend)
+	defer C.free(unsafe.Pointer(e))
+
+	batch_status := C.pbs_statserver(C.int(handle), a, e)
 
 	if batch_status == nil {
 		return nil, errors.New(Pbs_strerror(int(C.pbs_errno)))
@@ -411,11 +574,14 @@ func Pbs_statserver(handle int) ([]BatchStatus, error) {
 }
 
 func Pbs_strerror(errno int) string {
-    // char* from pbs_strerror is statically allocated, so can't be freed
-    return C.GoString(C.pbs_strerror(C.int(errno)))
+	// char* from pbs_strerror is statically allocated, so can't be freed
+	return C.GoString(C.pbs_strerror(C.int(errno)))
 }
 
-func Pbs_submit(handle int, script string, destination string, extend string) (string, error) {
+func Pbs_submit(handle int, attribs []Attrib, script string, destination string, extend string) (string, error) {
+	a := attrib2attribl(attribs)
+	defer freeattribl(a)
+
 	s := C.CString(script)
 	defer C.free(unsafe.Pointer(s))
 
@@ -425,7 +591,7 @@ func Pbs_submit(handle int, script string, destination string, extend string) (s
 	e := C.CString(extend)
 	defer C.free(unsafe.Pointer(e))
 
-	jobid := C.pbs_submit(C.int(handle), nil, s, d, e)
+	jobid := C.pbs_submit(C.int(handle), (*C.struct_attropl)(unsafe.Pointer(a)), s, d, e)
 	if jobid == nil {
 		return "", errors.New(Pbs_strerror(int(C.pbs_errno)))
 	}
---
+++ pbs_test.go
@@ -0,0 +1,231 @@
+package pbs
+
+import (
+	"os"
+	"os/user"
+	"testing"
+)
+
+var server = "localhost"
+var scriptPath = "test.sh"
+
+func logAttribute(t *testing.T, attr Attrib) {
+	if attr.Resource != "" {
+		t.Logf("  %s.%s: %s\n", attr.Name, attr.Resource, attr.Value)
+	} else {
+		t.Logf("  %s: %s\n", attr.Name, attr.Value)
+	}
+}
+
+func createSubmitScript(t *testing.T) {
+	fo, err := os.Create(scriptPath)
+	if err != nil {
+		t.Fatalf("Couldn't create submit script %s: %s", scriptPath, err)
+	}
+
+	defer func() {
+		if err := fo.Close(); err != nil {
+			t.Fatalf("Couldn't close submit script: %s\n", err)
+		}
+	}()
+
+	_, err = fo.WriteString("#!/bin/bash\nsleep 10\n")
+	if err != nil {
+		t.Fatalf("Couldn't write submit script: %s\n", err)
+	}
+
+	err = fo.Sync()
+	if err != nil {
+		t.Fatalf("Couldn't sync submit script: %s\n", err)
+	}
+}
+
+func getCurrentUser(t *testing.T) string {
+    user, err := user.Current()
+    if err != nil {
+        t.Fatalf("Couldn't get current user: %s", err)
+    }
+	return user.Username
+}
+
+func TestConnect(t *testing.T) {
+	handle, err := Pbs_connect(server)
+	if err != nil {
+		t.Fatalf("Connect to %s failed: %s\n", server, err)
+	}
+
+	errmsg := Pbs_geterrmsg(handle)
+	if errmsg != "" {
+		t.Fatalf("errmsg is: %s\n", errmsg)
+	}
+
+	err = Pbs_disconnect(handle)
+	if err != nil {
+		t.Errorf("Disconnecting failed: %s\n", err)
+	}
+
+	errmsg = Pbs_geterrmsg(handle)
+	if errmsg != "" {
+		t.Errorf("errmsg is: %s\n", errmsg)
+	}
+}
+
+func TestSubmit(t *testing.T) {
+	handle, err := Pbs_connect(server)
+	if err != nil {
+		t.Fatalf("Connect to %s failed: %s\n", server, err)
+	}
+
+	defer func() {
+		err = Pbs_disconnect(handle)
+		if err != nil {
+			t.Errorf("Disconnect failed: %s\n", err)
+		}
+	}()
+
+	createSubmitScript(t)
+	defer func() {
+		err := os.Remove(scriptPath)
+		if err != nil {
+			t.Fatalf("Couldn't remove submit script %s: %s", scriptPath, err)
+		}
+	}()
+
+	jobid, err := Pbs_submit(handle, []Attrib{}, scriptPath, "", "")
+	if err != nil {
+		t.Errorf("Job submission failed: %s\n", err)
+	} else {
+		t.Logf("Submitted jobid: %s\n", jobid)
+	}
+}
+
+func TestListJobs(t *testing.T) {
+	handle, err := Pbs_connect(server)
+	if err != nil {
+		t.Fatalf("Connect to %s failed: %s\n", server, err)
+	}
+
+	defer func() {
+		err = Pbs_disconnect(handle)
+		if err != nil {
+			t.Errorf("Disconnect failed: %s\n", err)
+		}
+	}()
+
+	filter := []Attrib{
+		Attrib{
+			Name:  ATTR_u,
+			Value: getCurrentUser(t),
+			Op:    EQ,
+		},
+	}
+	jobs, err := Pbs_selectjob(handle, filter, "")
+	if err != nil {
+	    t.Log(Pbs_geterrmsg(handle))
+		t.Fatalf("Couldn't get list of jobs: %s\n", err)
+	}
+
+	for _, id := range jobs {
+		j, err := Pbs_statjob(handle, id, []Attrib{}, "")
+		if err != nil {
+			t.Fatalf("Couldn't get job attributes for %s: %s\n", id, err)
+		}
+
+		for _, status := range j {
+			t.Logf("%s (%s)\n", status.Name, status.Text)
+			for _, attr := range status.Attributes {
+				logAttribute(t, attr)
+			}
+		}
+	}
+}
+
+func TestServerStatistics(t *testing.T) {
+	handle, err := Pbs_connect(server)
+	if err != nil {
+		t.Fatalf("Connect to %s failed: %s\n", server, err)
+	}
+
+	defer func() {
+		err = Pbs_disconnect(handle)
+		if err != nil {
+			t.Errorf("Disconnect failed: %s\n", err)
+		}
+	}()
+
+	attribs, err := Pbs_statserver(handle, nil, "")
+	if err != nil {
+		t.Errorf("Couldn't get server statistics: %s\n", err)
+	}
+
+	for _, server := range attribs {
+		t.Logf("%s (%s)\n", server.Name, server.Text)
+		for _, attr := range server.Attributes {
+			logAttribute(t, attr)
+		}
+	}
+}
+
+func TestQueueStatistics(t *testing.T) {
+	handle, err := Pbs_connect(server)
+	if err != nil {
+		t.Fatalf("Connect to %s failed: %s\n", server, err)
+	}
+
+	defer func() {
+		err = Pbs_disconnect(handle)
+		if err != nil {
+			t.Errorf("Disconnect failed: %s\n", err)
+		}
+	}()
+
+	queues, err := Pbs_statque(handle, "", []Attrib{}, "")
+	if err != nil {
+		t.Fatalf("Couldn't get queue statistics: %s\n", err)
+	}
+
+	for _, queue := range queues {
+		t.Logf("%s (%s)\n", queue.Name, queue.Text)
+		for _, attr := range queue.Attributes {
+			logAttribute(t, attr)
+		}
+	}
+}
+
+func TestResources(t *testing.T) {
+	handle, err := Pbs_connect(server)
+	if err != nil {
+		t.Fatalf("Connect to %s failed: %s\n", server, err)
+	}
+
+	defer func() {
+		err = Pbs_disconnect(handle)
+		if err != nil {
+			t.Errorf("Disconnect failed: %s\n", err)
+		}
+	}()
+
+	// See if required resources available
+	{
+		avail := Avail(handle, "nodes=1")
+		t.Logf("nodes=1: %s\n", avail)
+	}
+
+	// See how many nodes are present
+	{
+		total, err := Totpool(handle, 1)
+		if err != nil {
+			t.Fatalf("Couldn't get number of nodes: %s\n", err)
+		}
+		t.Logf("Total nodes: %d\n", total)
+	}
+
+	// See how many nodes are in use
+	{
+		use, err := Usepool(handle, 1)
+		if err != nil {
+			t.Fatalf("Couldn't get number of used nodes: %s", err)
+		}
+		t.Logf("Used nodes: %d\n", use)
+	}
+}
